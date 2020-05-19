c3926a4a6b5b61ec0f19c88e162263fb1dd2a0e8
github username <github email address>
2020-05-19 15:16:43 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 5276 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +6,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const form = document.getElementById("comment_form")
+
+fetch(imageURL)
+  .then(response => response.json())
+  .then(object => {
+    const imageTitle = document.getElementById('name')
+    imageTitle.innerText = object.name
+
+    const mainImage = document.getElementById('image')
+    mainImage.src = object.url
+
+    const likeNumber = document.getElementById('likes')
+    likeNumber.innerHTML = object.like_count
+
+    const likeButton = document.getElementById('like_button')
+    likeButton.addEventListener('click', (event)=> likeNumber.innerHTML++)
+
+  })
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target)
+  const comment = formData.get("comment")
+
+  let li = document.createElement("li")
+  
+  li.textContent = comment
+
+  document.body.append(li)
+  document.getElementById('comment_form').reset();
+  
 })
+
+// fetch(likeURL, {
+//   method: "POST",
+//   headers: {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json'
+//   },
+//   body: {
+//     image_id: 5276
+//     JSON.stringify({like_count})
+//   }
+  
+// })
